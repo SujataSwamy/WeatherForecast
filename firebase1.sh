28fd4b18c7bdb22ca54aa5ff9317d677023d8650
Sujata Swamy <sujata.swamy@airfi.aero>
2020-08-08 12:15:16 +0530

    modifications

--- firebase1.sh
+++ firebase1.sh
@@ -3,7 +3,6 @@
 # Firebase service account decrypt
 - openssl aes-256-cbc -K $encrypted_6ccb8a696b2d_key -iv $encrypted_6ccb8a696b2d_iv -in weatherforecast-d2654-4cca52839c71.json.enc -out /tmp/weatherforecast-d2654-4cca52839c71.json -d
 
-
 # Firebase setup
 wget --quiet --output-document=/tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz  
 mkdir -p /opt  
@@ -17,16 +16,14 @@ echo weatherforecast-d2654
 gcloud config set project weatherforecast-d2654
 
 # Activate cloud credentials
-
 gcloud auth activate-service-account --key-file /tmp/weatherforecast-d2654-4cca52839c71.json
 
 # List available options for logging purpose only (so that we can review available options)
-
 gcloud firebase test android models list
 gcloud firebase test android versions list
 
 ./gradlew build assembleDebug
-gcloud firebase test android run --app app/build/outputs/apk/debug/app-debug.apk --type=robo --device model=Nexus4,version=19
+gcloud firebase test android run --app app/build/outputs/apk/debug/app-debug.apk --type=robo --device model=Nexus4,version=26
 
 
 
