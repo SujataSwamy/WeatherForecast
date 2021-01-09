b56063ec5a5dc4a3009171dac82388408eb109d4
sujataswamy <sujatagopalswamy@gmail.com>
2021-01-10 00:21:16 +0530

    Mo

--- app/src/main/java/com/weatherforecast/data/network/APIRestService.kt
+++ app/src/main/java/com/weatherforecast/data/network/APIRestService.kt
@@ -1,10 +1,8 @@
 package com.weatherforecast.data.network
 
 import com.weatherforecast.domain.model.Pojo
-import com.weatherforecast.domain.model.UIState
 import retrofit2.http.GET
 import retrofit2.http.Query
-
 
 /**
  * This class is responsible for holding all APIs need to be invoked by the application.
@@ -19,7 +17,7 @@ interface WeatherRestService {
      * @param days     no. of days for forecast data
      * @return [<]
      */
-    //http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c9d1ca843e054a96b18a3851d805d2aa
+    // http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c9d1ca843e054a96b18a3851d805d2aa
     @GET("top-headlines?apiKey=c9d1ca843e054a96b18a3851d805d2aa")
     suspend fun getWeatherInfo(
         @Query("country") cityName: String?,
