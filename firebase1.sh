b36e2b5376c4ed9d7965e7fcc4e9b8ab6862f898
sujataswamy <sujatagopalswamy@gmail.com>
2021-01-10 00:10:13 +0530

    modi

--- firebase1.sh
+++ firebase1.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # Firebase service account decrypt
-- openssl aes-256-cbc -K $encrypted_6ccb8a696b2d_key -iv $encrypted_6ccb8a696b2d_iv -in weatherforecast-d2654-4cca52839c71.json.enc -out /tmp/weatherforecast-d2654-4cca52839c71.json -d
+#- openssl aes-256-cbc -K $encrypted_6ccb8a696b2d_key -iv $encrypted_6ccb8a696b2d_iv -in weatherforecast-d2654-4cca52839c71.json.enc -out /tmp/weatherforecast-d2654-4cca52839c71.json -d
 
 # Firebase setup
-wget --quiet --output-document=/tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz  
-mkdir -p /opt  
-tar zxf /tmp/google-cloud-sdk.tar.gz --directory /opt  
-/opt/google-cloud-sdk/install.sh --quiet
-source /opt/google-cloud-sdk/path.bash.inc
+#wget --quiet --output-document=/tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz
+#mkdir -p /opt
+#tar zxf /tmp/google-cloud-sdk.tar.gz --directory /opt
+#/opt/google-cloud-sdk/install.sh --quiet
+#source /opt/google-cloud-sdk/path.bash.inc
 
 # Setup and configure the project
-gcloud components update
-echo weatherforecast-d2654
-gcloud config set project weatherforecast-d2654
+#gcloud components update
+#echo weatherforecast-d2654
+#gcloud config set project weatherforecast-d2654
 
 # Activate cloud credentials
-gcloud auth activate-service-account --key-file /tmp/weatherforecast-d2654-4cca52839c71.json
+#gcloud auth activate-service-account --key-file /tmp/weatherforecast-d2654-4cca52839c71.json
 
 # List available options for logging purpose only (so that we can review available options)
-gcloud firebase test android models list
-gcloud firebase test android versions list
+#gcloud firebase test android models list
+#gcloud firebase test android versions list
 
 ./gradlew build assembleDebug
-gcloud firebase test android run --app app/build/outputs/apk/debug/app-debug.apk --type=robo --device model=Pixel2,version=28
+./gradlew ktlintFormat
+#gcloud firebase test android run --app app/build/outputs/apk/debug/app-debug.apk --type=robo --device model=Pixel2,version=28
 
 
 
